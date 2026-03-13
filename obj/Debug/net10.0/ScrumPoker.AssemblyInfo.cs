f061384024348e4de97ce57c42fa39f0f791237f
AndyStetts <andy.stettler@asirobots.com>
2026-03-13 08:52:03 -0600

    Launch settings and Scrum Poker Submission link

--- obj/Debug/net10.0/ScrumPoker.AssemblyInfo.cs
+++ obj/Debug/net10.0/ScrumPoker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScrumPoker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7327f158dd4ed1943d494c6a581479ba78e2bc35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51adfd7bbc3dfb5699025fb7c9dff08f0067dc56")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScrumPoker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScrumPoker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

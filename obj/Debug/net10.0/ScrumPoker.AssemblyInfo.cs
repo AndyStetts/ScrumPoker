7327f158dd4ed1943d494c6a581479ba78e2bc35
AndyStetts <andy.stettler@asirobots.com>
2026-03-12 22:13:49 -0600

    Assembly updates

--- obj/Debug/net10.0/ScrumPoker.AssemblyInfo.cs
+++ obj/Debug/net10.0/ScrumPoker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScrumPoker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29abc0646a94a8c726bfe6e3962f8e352e2150d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eeb566376203aad252e5cc10d686c10a2dcc4b8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScrumPoker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScrumPoker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
